2d28ea85078fa28c278d59f6d0eda8f6363a846e
KamranAlipour <kalipour@eng.ucsd.edu>
2022-03-31 16:47:10 -0700

    Add Enum for Op

--- ast.ts
+++ ast.ts
@@ -3,9 +3,11 @@ export type Stmt =
   | { tag: "define", name: string, value: Expr }
   | { tag: "expr", expr: Expr }
 
+export enum Op { Plus, Minus, Mul};
+
 export type Expr =
     { tag: "num", value: number }
   | { tag: "id", name: string }
-  | { tag: "biexp", name: string, value1: Expr, value2: Expr}
+  | { tag: "op", op: Op, left: Expr, right: Expr}
   | { tag: "builtin1", name: string, arg: Expr }
   | { tag: "builtin2", name: string, arg1: Expr , arg2: Expr}
